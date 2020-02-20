eda3cecfa676e2c62d2e54cb5540ebf01da0caf8
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-20 01:07:24 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -10,11 +10,6 @@ def add_character(show, name)
   while new_char < BASE_HOA.count do
     BASE_HOA[show] << name
     new_char += 1
-      upd_show = 0 
-      while upd_show < BASE_HOA.count do
-        return BASE_HOA
-        upd_show += 1 
-      end
   end
   
 end
